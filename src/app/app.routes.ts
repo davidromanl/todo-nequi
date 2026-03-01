cce22eaf7449292473e5fc0161594bf79be7348d
tailorsdev <roman.david@gmail.com>
2026-03-01 00:18:01 -0500

    Commit Inicial
    Add tasks & categories pages, models, and routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,16 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+    path: '',
+    redirectTo: 'tasks',
+    pathMatch: 'full',
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
+    path: 'tasks',
+    loadComponent: () => import('./pages/tasks/tasks.page').then(m => m.TasksPage)
+  },
+  {
+    path: 'categories',
+    loadComponent: () => import('./pages/categories/categories.page').then(m => m.CategoriesPage)
   },
 ];
